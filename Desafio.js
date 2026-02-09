5bd15307747843c2b7ebe97069c1f4a6db585570
Carina Cristina Teixeira De Souza <carinacristina1809@gmail.com>
2026-02-09 16:02:21 -0300

    Add files via upload

---
+++ Desafio.js
@@ -0,0 +1,55 @@
+
+//Desafio 01
+let meuNome = "Carina";
+var idade = 16;
+console.log(meuNome,idade);
+
+//Desafio 02
+let comidaFavorita = "Musse de Maracujá"
+//parte que vai dar erro no valor
+comidaFavorita = "Morango do amor"
+const paisNascimento ="Brasil"
+//parte que vai dar erro no valor 
+paisNascimento = " Espanha"
+console.log(comidaFavorita);
+
+//Desafio 03
+let nome = "Carlos";
+var idade = "16";
+var cidade ="São Paulo";
+console.log(nome,idade,cidade);
+
+//Desafio 04
+let curso = "moda";
+curso = "nutrição";
+console.log(curso);
+
+//Desafio 05
+const anoAtual ="2026";
+anoAtual = "2009";
+console.log(anoAtual);
+
+//Desafio 06
+let nomePessoa = "Nicolas";
+let sobrenome = "Younc"
+console.log(nomePessoa,sobrenome);
+
+//Desafio 07
+let usuario = "Maki";
+let email = "makimakizenin@gmail.com";
+var idadePessoa ="São Paulo";
+console.log(usuario,email,idadePessoa);
+
+//Desafio 08
+var numeroDaSorte = "22";
+var numeroDoAzar = "13";
+console.log(numeroDaSorte,numeroDoAzar);
+//Desafio 09
+var notaBaixa = "5";
+var notaMedia = "7";
+var notaAlta = "10";
+console.log(notaBaixa,notaMedia,notaBaixa);
+//Desafio 10
+ let produto = "batom";
+ var preço = "R$ 15,00";
+ console.log(produto,preço);
